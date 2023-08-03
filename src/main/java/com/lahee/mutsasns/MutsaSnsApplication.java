79e770b787f56e40325a49e9fea404e39a69f28c
Juhee Park <jouhi3322@gmail.com>
2023-08-03 10:53:30 +0900

    init project

---
+++ src/main/java/com/lahee/mutsasns/MutsaSnsApplication.java
@@ -0,0 +1,13 @@
+package com.lahee.mutsasns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MutsaSnsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MutsaSnsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lahee/mutsasns/MutsaSnsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lahee.mutsasns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MutsaSnsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
