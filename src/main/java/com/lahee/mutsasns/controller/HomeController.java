ac8992af69826d5a29d9de64c5c87cf5107857ed
Juhee Park <jouhi3322@gmail.com>
2023-08-21 23:37:04 +0900

    project : 프로젝트 세팅

---
+++ src/main/java/com/lahee/mutsasns/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.lahee.mutsasns.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+@Slf4j
+public class HomeController {
+    @GetMapping
+    public String home() {
+        return "home";
+    }
+}
